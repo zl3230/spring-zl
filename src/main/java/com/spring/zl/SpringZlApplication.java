2ad653a41167ea924e3188c25b3b674bb429610d
张家人 <blf53517230>
2021-05-22 22:44:54 +0800

    Initial commit

---
+++ src/main/java/com/spring/zl/SpringZlApplication.java
@@ -0,0 +1,13 @@
+package com.spring.zl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/zl/SpringZlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.zl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringZlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
